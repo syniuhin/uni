2587f95ea7423de5e4d0d09e3b6c6c9b7901adf9
Eli Syniuhin <elisyniuhin@asana.com>
2017-11-29 20:49:57 -0800

    [Haskell] ApplicativeOnMonoid (OMG it was painful)

--- haskell/btree/applicative_with_default.hs
+++ haskell/btree/applicative_with_default.hs
@@ -1,26 +1,26 @@
 module BinaryTreeApplicativeWithDefault where
-import Prelude hiding ((<*>))
 import BinaryTree
 import BinaryTreeFunctor
-import Data.Monoid
 
 instance Monoid Int where
   mempty = 0
   mappend = (+)
 
 class Functor f => ApplicativeOnMonoid f where
-  pure :: (Monoid x) => x -> f x
-  (<*>) :: (Monoid a) => f (a -> b) -> f a -> f b
+  pure :: x -> f x
+  (<**>) :: (Monoid a) => f (a -> a) -> f a -> f a
+
+infixl 4 <**>
 
 instance ApplicativeOnMonoid BTree where
   pure x = Branch x Empty Empty
-  Empty <*> Empty = Empty
-  Empty <*> (Branch x left right) = Empty
-  (Branch f fleft fright) <*> Empty = Branch (f mempty) (fleft <*> Empty) (fright <*> Empty)
-  (Branch f fleft fright) <*> (Branch y left right) = Branch (f y) (fleft <*> left) (fright <*> right)
+  Empty <**> Empty = Empty
+  Empty <**> (Branch x left right) = Branch x left right
+  (Branch f fleft fright) <**> Empty = Branch (f mempty) (fleft <**> Empty) (fright <**> Empty)
+  (Branch f fleft fright) <**> (Branch y left right) = Branch (f y) (fleft <**> left) (fright <**> right)
 
 -- let a = Branch 1 (Branch 2 (Branch 3 Empty Empty) Empty) (Branch 4 Empty Empty)
 -- let b = Branch 1 (Branch 4 Empty Empty) (Branch 2 Empty (Branch 3 Empty Empty))
 -- let c = Branch 1 (Branch 2 (Branch 3 Empty Empty) (Branch 4 Empty Empty)) Empty
--- (+) <$> a <*> b
+-- (+) <$> a <**> b
 ---- Branch 2 (Branch 6 (Branch 3 Empty Empty) Empty) (Branch 6 Empty (Branch 3 Empty Empty))
