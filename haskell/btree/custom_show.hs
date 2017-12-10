83c949e61e7d80bc5d210c1cac9b3a69b40f38ea
Eli Syniuhin <elisyniuhin@asana.com>
2017-12-10 15:59:29 -0800

    [Haskell] Custom show finished

--- haskell/btree/custom_show.hs
+++ haskell/btree/custom_show.hs
@@ -6,32 +6,64 @@ data BTree a = Empty | Branch a (BTree a) (BTree a)
 
 instance (Show a) => Show (BTree a) where
   show Empty = "*"
-  show t = intercalate "\n" (levelWiseTraversal $ subtreeWidth t)
+  show t =
+    let treeInfo = subtreeWidth t 0
+        levels = levelWiseWalk treeInfo []
+        res = intercalate "\n" levels
+    in res
 
 -- (repr, width, margin)
 type TShowable = (String, Int, Int)
-
+-- tree -> margin -> tree [2]
 subtreeWidth :: (Show a) => BTree a -> Int -> BTree TShowable
-subtreeWidth Empty margin = Branch ("*", 2, margin) Empty Empty
+subtreeWidth Empty margin = Branch ("* ", 2, margin) Empty Empty
 subtreeWidth (Branch x left right) margin =
     let leftTree = subtreeWidth left margin
-        rightTree = case leftTree of
-          (Branch (_, width, margin)) -> subtreeWidth right (margin + width)
-        xwidth = case (leftTree, rightTree) of
-          (Branch (_, leftWidth, leftMargin) _ _, Branch (_, rightWidth) _ _) -> max (1 + (length $ show x)) (leftWidth + rightWidth)
-        res = (Branch (show x ++ " ", xwidth) leftTree rightTree)
-    in res
+        xStr = show x ++ " "
+        currentWidth = case leftTree of
+            (Branch (_, leftWidth, _) _ _) -> max (length xStr) leftWidth
+        rightTree = subtreeWidth right (margin + currentWidth)
+        xWidth = case rightTree of
+          (Branch (_, rightWidth, _) _ _) -> currentWidth + rightWidth
+    in Branch (xStr, xWidth, margin) leftTree rightTree
 
-concatLevels :: [String] -> [String] -> [String]
-concatLevels [] [] = []
-concatLevels xs [] = xs
-concatLevels [] xs = xs
-concatLevels (x:xs) (y:ys) =
-    [x ++ y] ++ (concatLevels xs ys)
-
-levelWiseTraversal :: BTree TShowable -> [String]
-levelWiseTraversal (Empty) = []
-levelWiseTraversal (Branch (x, xw) left right) =
-    [printf ("%-" ++ (show xw) ++ "s") x] ++ concatLevels (levelWiseTraversal left) (levelWiseTraversal right)
+-- Fill the traversal level by level.
+-- tree -> current level -> levels from current to the bottom -> modified levels from current to the bottom
+levelWiseWalk :: BTree TShowable -> [String] -> [String]
+levelWiseWalk Empty levels = levels
+levelWiseWalk (Branch (xStr, xWidth, xMargin) left right) levels =
+    let (currentLevel, nextLevels) = case levels of
+            (x:xs) -> (x, xs)
+            _ -> ("", [])
+        leftMargin = max (xMargin - (length currentLevel)) 0
+        currentLevelModified = currentLevel ++ printf "%*s%s" leftMargin "" xStr
+        nextLevelsModified = levelWiseWalk right (levelWiseWalk left nextLevels)
+    in currentLevelModified : nextLevelsModified
 
 -- putStrLn $ show $ Branch 1 Empty (Branch 2 (Branch 3 Empty Empty) (Branch 4 Empty Empty))
+---- 1
+---- * 2
+----   3   4
+----   * * * *
+-- putStrLn $ show $ Branch 1 (Branch 2 (Branch 3 Empty (Branch 4 Empty Empty)) Empty) (Branch 5 Empty Empty)
+---- 1
+---- 2       5
+---- 3     * * *
+---- * 4
+----   * *
+-- putStrLn $ show $ Branch 1 (Branch 2 (Branch 4 Empty Empty) Empty) (Branch 3 (Branch 5 (Branch 7 Empty Empty) Empty) (Branch 6 (Branch 8 (Branch 10 Empty Empty) (Branch 11 Empty Empty)) (Branch 9 Empty Empty)))
+---- 1
+---- 2     3
+---- 4   * 5     6
+---- * *   7   * 8         9
+----       * *   10   11   * *
+----             *  * *  *
+-- putStrLn $ show $ Branch 1 Empty (Branch 2 Empty (Branch 3 (Branch 4 (Branch 5 (Branch 6 Empty (Branch 7 Empty Empty)) Empty) Empty) Empty))
+---- 1
+---- * 2
+----   * 3
+----     4         *
+----     5       *
+----     6     *
+----     * 7
+----       * *
