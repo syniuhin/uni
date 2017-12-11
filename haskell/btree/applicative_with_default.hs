1943e006c31f8032b31cdc18d174f2ba839462a9
Eli Syniuhin <elisyniuhin@asana.com>
2017-12-10 16:22:41 -0800

    WIP

--- haskell/btree/applicative_with_default.hs
+++ haskell/btree/applicative_with_default.hs
@@ -8,16 +8,16 @@ instance Monoid Int where
 
 class Functor f => ApplicativeOnMonoid f where
   pure :: x -> f x
-  (<**>) :: (Monoid a) => f (a -> a) -> f a -> f a
+  (<*>) :: (Monoid a) => f (a -> a) -> f a -> f a
 
 infixl 4 <**>
 
 instance ApplicativeOnMonoid BTree where
   pure x = Branch x Empty Empty
-  Empty <**> Empty = Empty
-  Empty <**> (Branch x left right) = Branch x left right
-  (Branch f fleft fright) <**> Empty = Branch (f mempty) (fleft <**> Empty) (fright <**> Empty)
-  (Branch f fleft fright) <**> (Branch y left right) = Branch (f y) (fleft <**> left) (fright <**> right)
+  Empty <*> Empty = Empty
+  Empty <*> (Branch x left right) = Branch x left right
+  (Branch f fleft fright) <*> Empty = Branch (f mempty) (fleft <**> Empty) (fright <**> Empty)
+  (Branch f fleft fright) <*> (Branch y left right) = Branch (f y) (fleft <**> left) (fright <**> right)
 
 -- let a = Branch 1 (Branch 2 (Branch 3 Empty Empty) Empty) (Branch 4 Empty Empty)
 -- let b = Branch 1 (Branch 4 Empty Empty) (Branch 2 Empty (Branch 3 Empty Empty))
