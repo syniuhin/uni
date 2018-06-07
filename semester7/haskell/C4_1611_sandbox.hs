372bb18f832c9f16f169b8d248a20c5c5dd5c74a
Eli Syniuhin <elisyniuhin@asana.com>
2018-06-07 19:26:52 +0300

    [Haskell] 90

--- semester7/haskell/C4_1611_sandbox.hs
+++ semester7/haskell/C4_1611_sandbox.hs
@@ -4,18 +4,19 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 import Control.Monad.Trans.State
+import Data.Ratio
 
 -- Kleisli arrow
 -- Left identity: return >=> g	= g
 -- Right identity:	f >=> return = f
 -- Associativity: (f >=> g) >=> h = f >=> (g >=> h)
-class Kleisli k where
+class Applicative k => Kleisli k where
 	(>=>)::(a -> k b) -> (b -> k c) -> (a -> k c)
 	(<=<)::(b -> k c) -> (a -> k b) -> (a -> k c)
 	return':: a->k a
 	(<=<) = flip (>=>)
 	(>=>) = flip (<=<)
-
+	-- {-# MINIMAL (<=<) | (>=>) #-}
 
 infixl 9 >=>
 infixl 9 <=<
@@ -23,12 +24,14 @@ infixl 9 <=<
 instance Kleisli [] where
 	f >=> g 	= concat . (map g) . f
 	return' a 	= [a]
+
 -- instance Kleisli  ((,)String) where
 -- 	return' a = ("", a)
 -- 	f >=> g   = \x -> let (sy, y) = f x;
 -- 						  (sz, z) = g y
 -- 					  in (sy ++ sz, z)
 
+-- 31
 instance Monoid m => Kleisli ((,) (m)) where
 	return' a = (mempty, a)
 	f >=> g   = \x -> let (sy, y) = f x;
@@ -51,7 +54,48 @@ x = 3
 y = f . g $ x
 (log_y2, y2) = f_log <=< g_log $ x
 
--- Kleisli arrow based on monoids
+-- 32 ??
+-- instance Kleisli k => Monad k where
+--     ma >>= f = fmap f ma
+
+-- 33 ??
+-- instance Monad m => Kleisli m where
+--     f >=> g = fmap (\x -> f . g $ x)
+
+-- 34
+class Applicative f => Flatten f where
+    flatten :: (a -> f (f b)) -> (a -> f b)
+
+instance Flatten [] where
+    flatten f = \x ->
+        let y = f x
+            z = case y of
+              [] -> []
+              [xs] -> xs
+        in z
+
+wrapp :: Int -> [[Int]]
+wrapp x
+    | x == 10 = []
+    | otherwise = [[x]]
+
+instance Flatten Maybe where
+    flatten f = \x ->
+        let y = f x
+            z = case y of
+                Nothing -> Nothing
+                Just x -> x
+        in z
+
+veryMaybe :: Int -> Maybe (Maybe Int)
+veryMaybe x
+    | x == 10 = Nothing
+    | x == 11 = Just Nothing
+    | otherwise = Just (Just (x * 2))
+
+-- 35
+-- instance Flatten f => Monad f where
+--     ma >>= f =
 
 -- Maybe monad - Parents example
 data Person = Joe | Jim | Linda | Keysi | Fedor | Anya deriving (Show, Eq)
@@ -69,85 +113,30 @@ mother Linda = Just Keysi
 mother Keysi = Just Anya
 mother _ = Nothing
 
+-- 37
+parents :: Person -> [Person]
+parents p =
+    case (father p, mother p) of
+        (Nothing, Nothing) -> []
+        (Just f, Nothing) -> [f]
+        (Nothing, Just m) -> [m]
+        (Just f, Just m) -> [f, m]
 
-maternalGrandfather :: Person -> Maybe Person
-maternalGrandfather p =
-    case mother p of
-        Nothing -> Nothing
-        Just mom -> father mom
+allGrandparents :: Person -> [Person]
+allGrandparents p = concat (map parents (parents p))
 
-bothGrandfathers :: Person -> Maybe (Person, Person)
-bothGrandfathers p =
-    case father p of
-        Nothing -> Nothing
-        Just dad ->
-            case father dad of
-                Nothing -> Nothing
-                Just gf1 ->                          -- found first grandfather
-                    case mother p of
-                        Nothing -> Nothing
-                        Just mom ->
-                            case father mom of
-                                Nothing -> Nothing
-                                Just gf2 ->          -- found second grandfather
-                                    Just (gf1, gf2)
+-- 38
+traverseParents :: [Person -> Maybe Person] -> Person -> Maybe Person
+traverseParents [] _ = Nothing
+traverseParents [f] p = f p
+traverseParents (f:fs) p = f p >>= traverseParents fs
 
+-- 39
+-- newtype Prob a = Prob { getProb :: [(a,Rational)] } deriving Show
+-- instance Functor Prob where
+--     fmap f (Prob xs) = Prob $ map (\(x,p) -> (f x,p)) xs
 
-bothGrandfathersII p = do {
-        dad <- father p;
-        gf1 <- father dad;
-        mom <- mother p;
-        gf2 <- father mom;
-        return (gf1, gf2);
-      }
-
---bothGrandfathersIII:: Person ->Maybe (Person, Person)
-bothGrandfathersIII p =
-    (father p >>= father)
-
-
-bothGrandfathersIV p =
-       (father p >>= father) >>=
-           (\gf1 -> (mother p >>= father) >>=
-               (\gf2 -> return (gf1,gf2) ))
-
-bothGrandfathers' p = do
-    f <- father p
-    gf1 <- father f
-    gf2 <- (mother p >>= father)
-    return (gf1,gf2)
-
-
-
-
--- Lists as monads
-doList = do
-    a <- [1,2,3]
-    return a
-
-doList' =
-    [1,2,3] >>= return
-
-h 0 = []
-h n = n:(h (n-1))
-
-doList2 = do
-    a <- [1,2,3]
-    h a
-
-doList2' =
-    [1,2,3] >>= h
-
-
-doList3 = do
-    a <- [1,2,3]
-    b <- [3,4]
-    return (a,b)
-
-
-doList3' =
-    [1,2,3] >>= (
-            \a -> [3,4] >>= (
-                \b -> return (a * b)
-            )
-        )
+-- instance Monad Prob where
+--     return x = Prob [(x,1%1)]
+--     m >>= f = flatten (fmap f m)
+--     fail _ = Prob []
